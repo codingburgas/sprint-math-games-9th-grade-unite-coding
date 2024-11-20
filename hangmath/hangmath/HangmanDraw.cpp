bf6797424190e5cac49ad541861233cee53e0549
unknown <AGKostov23@codingburgas.bg>
2024-11-20 21:04:42 +0200

    Add changes

--- hangmath/hangmath/HangmanDraw.cpp
+++
@@ -1,7 +0,0 @@
-/*#include <iostream>
-#include "PrintMessage.h"
-using namespace std;
-
-void DrawHangman(int guessCount) {
-	PrintMessage("|", false, false);
-}*/
--- hangmath/hangmath/PrintMessage.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef PrintMessage_H
-#define PrintMessage_H
-
-int PrintMessage(string message, bool HmTop, bool HmBottom);
-
-#endif
